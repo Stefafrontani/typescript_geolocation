422442eadfbfe24f5268e7b81e3d34056c08f340
Stefano Frontani <stefano.frontani@globant.com>
2020-05-29 21:27:06 -0300

    Company - Add class

--- src/Company.ts
+++ src/Company.ts
@@ -2,7 +2,7 @@ import faker from "faker";
 
 export class Company {
     companyName: string;
-    catchPhrase: string,
+    catchPhrase: string;
     location: {
         lat: number;
         lng: number;
